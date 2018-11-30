08ca6dba1f5f79cc75da03728377b32b870fe2f3
Carlos Escura <carlosescura@gmail.com>
2018-11-30 16:56:25 +0100

    V2-Finished

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import re
 app = Flask(__name__)
 
 
+# Precompile all regex to obtain a performance increase
 _re_processor = re.compile(r'processor\s+: (\d+)')
+_re_vendor_id = re.compile(r'vendor_id\s+: (.*)')
+_re_family = re.compile(r'family\s+: (.*)')
+_re_model = re.compile(r'model\s+: (.*)')
+_re_model_name = re.compile(r'model name\s+: (.*)')
+_re_stepping = re.compile(r'stepping\s+: (.*)')
+_re_cpu_MHz = re.compile(r'cpu MHz\s+: (.*)')
+_re_cache_size = re.compile(r'cache size\s+: (.*)')
+_re_physical_id = re.compile(r'physical id\s+: (.*)')
+_re_core_id = re.compile(r'core id\s+: (.*)')
+_re_cpu_cores = re.compile(r'cpu cores\s+: (.*)')
+_re_siblings = re.compile(r'siblings\s+: (.*)')
+_re_flags = re.compile(r'flags\s+: (.*)')
 
 
-class CPU(object):
+class Processor(object):
     def __init__(self):
         self.vendor_id = ''
         self.family = ''
@@ -22,6 +35,9 @@ class CPU(object):
         self.cores = ''
         self.siblings = ''
         self.flags = ''
+
+    def toDict(self):
+        return self.__dict__
 
 
 def get_num_cpu_sockets(processors):
@@ -61,63 +77,62 @@ def get_cpuinfo():
     current_processor = None
     with open('/proc/cpuinfo') as f:
         for line in f.readlines():
-            m = re.match(r'processor\s+: (\d+)', line)
+            m = _re_processor.match(line)
             if m:
-                current_processor = CPU()
+                current_processor = Processor()
                 processors[m.group(1)] = current_processor
 
-            m = re.match(r'vendor_id\s+: (.*)', line)
+            m = _re_vendor_id.match(line)
             if m:
                 current_processor.vendor_id = m.group(1)
 
-            m = re.match(r'family\s+: (.*)', line)
+            m = _re_family.match(line)
             if m:
                 current_processor.family = m.group(1)
 
-            m = re.match(r'model\s+: (.*)', line)
+            m = _re_model.match(line)
             if m:
                 current_processor.model = m.group(1)
 
-            m = re.match(r'model name\s+: (.*)', line)
+            m = _re_model_name.match(line)
             if m:
                 current_processor.model_name = m.group(1)
 
-            m = re.match(r'stepping\s+: (.*)', line)
+            m = _re_stepping.match(line)
             if m:
                 current_processor.stepping = m.group(1)
 
-            m = re.match(r'cpu MHz\s+: (.*)', line)
+            m = _re_cpu_MHz.match(line)
             if m:
                 current_processor.mhz = m.group(1)
 
-            m = re.match(r'cache size\s+: (.*)', line)
+            m = _re_cache_size.match(line)
             if m:
                 current_processor.cache_size = m.group(1)
 
-            m = re.match(r'physical id\s+: (.*)', line)
+            m = _re_physical_id.match(line)
             if m:
                 current_processor.physical_id = m.group(1)
 
-            m = re.match(r'core id\s+: (.*)', line)
+            m = _re_core_id.match(line)
             if m:
                 current_processor.core_id = m.group(1)
 
-            m = re.match(r'cpu cores\s+: (.*)', line)
+            m = _re_cpu_cores.match(line)
             if m:
                 current_processor.cores = m.group(1)
 
-            m = re.match(r'siblings\s+: (.*)', line)
+            m = _re_siblings.match(line)
             if m:
                 current_processor.siblings = m.group(1)
 
-            m = re.match(r'flags\s+: (.*)', line)
+            m = _re_flags.match(line)
             if m:
-                flags = m.group(1).split()
-                current_processor.flags = flags
+                current_processor.flags = m.group(1).split()
 
     if len(processors):
         for p in processors:
-            processors[p] = processors[p].__dict__
+            processors[p] = processors[p].toDict()
         return calculate_core_stats(processors)
 
     return processors
